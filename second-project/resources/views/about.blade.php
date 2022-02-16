618c446332c4893053a5eaf882010aa728a1eb5d
ohahiduzzaman1998 <ohahiduzzaman1998@gmail.com>
2022-02-16 13:05:37 +0600

    laravel full name

---
+++ second-project/resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+
+@section('title')
+    About page
+@endsection
+@section('body')
+    <h1>About page</h1>
+@endsection
---
+++ second-project/resources/views/demo.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+
+@section('title')
+    BITM page
+@endsection
+
+@section('body')
+    <h1>BITM page</h1>
+@endsection
